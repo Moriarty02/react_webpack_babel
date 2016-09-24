b4eb8cd6edf5d05361d8c806b674da39c5eb95fa
zhangxiuhua_company_PC <zhangxiuhua@360.cn>
2016-09-24 17:16:34 +0800

    weibo  example succeed

---
+++ weibo/js/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Publisher from "./component/publisher/publisher.jsx";
+
+ReactDOM.render( <
+    Publisher / > , document.getElementById("container")
+);
